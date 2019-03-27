f99e47b7627c22e60e22397cc7094dd3899f060b
austinfarrar <afarrar@mail.umw.edu>
2019-03-27 09:18:10 -0400

    This is my program for N5

---
+++ NumberTheory/N5_Divisors_Farrar.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 15:02:49 2019
+
+@author: Owner
+"""
+
+
+divisors = []
+
+def divide(n):
+    for ii in range(1,n):
+        if n % ii == 0:
+            divisors.append(ii)
+
+
+    print(divisors)
+
+divide(10)
