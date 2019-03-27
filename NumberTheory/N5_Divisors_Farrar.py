f937b2ef5b719276ac75dc924fb6f4ddad8e0bd7
austinfarrar <afarrar@mail.umw.edu>
2019-03-27 12:09:13 -0400

    final updated N5

--- NumberTheory/N5_Divisors_Farrar.py
+++ NumberTheory/N5_Divisors_Farrar.py
@@ -6,14 +6,11 @@ Created on Tue Mar 26 15:02:49 2019
 """
 
 
-divisors = []
-
 def divide(n):
+    divisors = []
     for ii in range(1,n):
         if n % ii == 0:
             divisors.append(ii)
+    return divisors
 
-    print('n =', n)
-    print('the divisors are',divisors)
-
-divide(10)
+print(divide(100))
